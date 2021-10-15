e1184bfd60389af40e7f7666da2194e4ac28f3d9
MarshallJ27 <37240959+MarshallJ27@users.noreply.github.com>
2021-10-14 19:13:22 -0700

    Updated AG-grid
    Updated graph

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { Component } from 'react'
 import {AgGridColumn, AgGridReact} from 'ag-grid-react';
-
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-   const rowData = [
-       {Name: "Marshall", Phone_number: "360-921-2222", Task:'do dishes', Priority_level: '3', Status: 'Solved'},
-       {Name: "Marshall", Phone_number: "360-921-2222", Task:'do dishes', Priority_level: '3', Status: 'Solved'},
-       {Name: "Marshall", Phone_number: "360-921-2222", Task:'do dishes', Priority_level: '3', Status: 'Solved'},
-       {Name: "Marshall", Phone_number: "360-921-2222", Task:'do dishes', Priority_level: '3', Status: 'Solved'},
-       {Name: "Marshall", Phone_number: "360-921-2222", Task:'do dishes', Priority_level: '3', Status: 'Solved'},
-       {Name: "Marshall", Phone_number: "360-921-2222", Task:'do dishes', Priority_level: '3', Status: 'Solved'},
-       {Name: "Marshall", Phone_number: "360-921-2222", Task:'do dishes', Priority_level: '3', Status: 'Solved'},
-   ];
+    const rowData = [
+        {Task: "Toyota", Worker: "Celica", start_time: 3500, priority: 'high'},
+        {Task: "Ford", Worker: "Mondeo", start_time: 32000, priority: 'med'},
+        {Task: "Porsche", Worker: "Boxter", start_time: 72000, priority: 'low'}
+    ];
 
-   export default function RowAndColumnSpacing() {
-   return (
-       <div className="ag-theme-alpine" style={{height: 400, width: 1000}}>
-           <AgGridReact
-               rowData={rowData}>
-               <AgGridColumn field="Name"></AgGridColumn>
-               <AgGridColumn field="Phone_number"></AgGridColumn>
-               <AgGridColumn field="Task"></AgGridColumn>
-               <AgGridColumn field="Priority_level"></AgGridColumn>
-               <AgGridColumn field="Status"></AgGridColumn>
-           </AgGridReact>
-       </div>
-   );
-};
+    const rowClassRules = {
+        'rag-green': 'data.priority = low',
+        'rag-yellow': 'data.priority = med',
+        'rag-red': 'data.priority = high',
+    };
+
+ 
+
+
+class mainGrid extends Component {
+    
+
+
+
+
+    render() {
+      return (
+        <div className="ag-theme-alpine" style={{height: '100%', width: '70%'}}>
+        <AgGridReact
+            rowSelection = 'multiple'
+            rowData={rowData}>
+            <AgGridColumn field="Task" rowDrag={true} checkboxSelection={true}></AgGridColumn>
+            <AgGridColumn field="Worker" sortable={true}></AgGridColumn>
+            <AgGridColumn field="start_time"></AgGridColumn>
+            <AgGridColumn field="Priority"></AgGridColumn>
+            <AgGridColumn field="Complete Time"></AgGridColumn>
+        </AgGridReact>
+    </div>
+    );
+}
+}
+
+export default mainGrid;
